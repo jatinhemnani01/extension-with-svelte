3c1f84f920ee2e7d9598d524930406a757727545
Van Vuong Ngo <vanvuong.ngo@jobcloud.ch>
2021-06-08 03:29:06 +0200

    patch1: remove extensions file into public; update packages and remove cpx package; chore svelte app

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script>
+  export let name;
   let count = 0;
 </script>
 
-<h1>{count}</h1>
+<h1>{name}</h1>
+<p>{count}</p>
 <button
   on:click={() => {
     count++;
-  }}>Increment</button
->
+  }}
+>Increment</button>
